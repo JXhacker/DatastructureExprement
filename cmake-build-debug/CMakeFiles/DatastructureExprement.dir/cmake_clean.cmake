c112672bf99bd4d176ef6095a678de76b61ac8aa
wanzhiwen <977242261@qq.com>
2018-10-28 14:51:41 +0800

    init repo

---
+++ cmake-build-debug/CMakeFiles/DatastructureExprement.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DatastructureExprement.dir/main.cpp.obj"
+  "DatastructureExprement.pdb"
+  "DatastructureExprement.exe"
+  "DatastructureExprement.exe.manifest"
+  "libDatastructureExprement.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DatastructureExprement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
